eade1088e62a216663c99745e1c57c1d746b0575
danyvs <daniel.sava99@protonmail.com>
2020-02-06 01:32:46 +0200

    Documentatie 2

--- 2.R
+++ 2.R
@@ -1,8 +1,3 @@
-func <- function(x, a) {
-  x ^ (a - 1) * exp(-x)
-}
-
-
 fgam <- function(a) {
   if (a == 1) {
     1
@@ -14,7 +9,9 @@ fgam <- function(a) {
     (a - 1) * fgam(a - 1)
   }
   else {
-    integrate(func, 0, Inf, a)$value
+    integrate(function(x, a) {
+      x ^ (a - 1) * exp(-x)
+    }, 0, Inf, a)$value
   }
 }
 
@@ -121,41 +118,88 @@ fprob9 <- function(a, b) {
 }
 
 
+fsyssum <- Vectorize(function(x, a, b) {
+  integrate(function(t) {
+    dgamma(x - t, shape = a, scale = b) * dbeta(t, a, b)
+  }, 0, 1)$value
+})
+
+
+fsysprob7 <- function(a, b) {
+  integrate(fsyssum, 0, 5, a, b)$value
+}
+
+
+fsysdiff <- Vectorize(function(x, a, b) {
+  integrate(function(t) {
+    dgamma(x - t, a, b) * dbeta(t, a, b)
+  }, 0, 1)$value
+})
+
+
+fsysprob8 <- function(a, b) {
+  integrate(fsysdiff, 0.5, Inf, a, b)$value
+}
+
+
+fsysprob9 <- function(a, b) {
+  integrate(fsyssum, 3, Inf, a, b)$value / integrate(fsysdiff, 0.5, Inf, a, b)$value
+}
+
+
 print_results <- function(a, b) {
-  results <- matrix(nrow = 9, ncol = 2)
-  rownames(results) <- 1:9
-  colnames(results) <- c("User", "System")
+  results <- matrix(nrow = 9, ncol = 3)
+  colnames(results) <- c("Nr.", "User", "System")
   
-  results[1, 1] <- fprobgamma1(a, b)
-  results[1, 2] <- pgamma(3, shape = a, scale = b)
+  results[1, 1] <- "1"
+  results[1, 2] <- fprobgamma1(a, b)
+  results[1, 3] <- pgamma(3, shape = a, scale = b)
   
-  results[2, 1] <- fprobgamma2(a, b)
-  results[2, 2] <- pgamma(5, shape = a, scale = b) - pgamma(2, shape = a, scale = b)
+  results[2, 1] <- "2"
+  results[2, 2] <- fprobgamma2(a, b)
+  results[2, 3] <- pgamma(5, shape = a, scale = b) - pgamma(2, shape = a, scale = b)
   
-  results[3, 1] <- fprobgamma3(a, b)
-  results[3, 2] <- (pgamma(4, shape = a, scale = b) - pgamma(3, shape = a, scale = b)) /
+  results[3, 1] <- "3"
+  results[3, 2] <- fprobgamma3(a, b)
+  results[3, 3] <- (pgamma(4, shape = a, scale = b) - pgamma(3, shape = a, scale = b)) /
     pgamma(2, shape = a, scale = b, lower.tail = FALSE)
   
-  results[4, 1] <- fprobbeta4(a, b)
-  results[4, 2] <- pbeta(2, shape1 = a, shape2 = b, lower.tail = FALSE)
+  results[4, 1] <- "4"
+  results[4, 2] <- fprobbeta4(a, b)
+  results[4, 3] <- pbeta(2, shape1 = a, shape2 = b, lower.tail = FALSE)
   
-  results[5, 1] <- fprobbeta5(a, b)
-  results[5, 2] <- pbeta(6, shape1 = a, shape2 = b) - pbeta(4, shape1 = a, shape2 = b)
+  results[5, 1] <- "5"
+  results[5, 2] <- fprobbeta5(a, b)
+  results[5, 3] <- pbeta(6, shape1 = a, shape2 = b) - pbeta(4, shape1 = a, shape2 = b)
   
-  results[6, 1] <- fprobbeta6(a, b)
-  results[6, 2] <- (pbeta(1, shape1 = a, shape2 = b) - pbeta(0, shape1 = a, shape2 = b)) / 
+  results[6, 1] <- "6"
+  results[6, 2] <- fprobbeta6(a, b)
+  results[6, 3] <- (pbeta(1, shape1 = a, shape2 = b) - pbeta(0, shape1 = a, shape2 = b)) / 
     pbeta(7, shape1 = a, shape2 = b)
   
-  results[7, 1] <- fprob7(a, b)
+  results[7, 1] <- "7"
   results[7, 2] <- fprob7(a, b)
+  results[7, 3] <- fsysprob7(a, b)
   
-  results[8, 1] <- fprob8(a, b)
+  results[8, 1] <- "8"
   results[8, 2] <- fprob8(a, b)
+  results[8, 3] <- fsysprob8(a, b)
   
-  results[9, 1] <- fprob9(a, b)
+  results[9, 1] <- "9"
   results[9, 2] <- fprob9(a, b)
+  results[9, 3] <- fsysprob9(a, b)
   
   results
 }
 
-print_results(3, 2)
+
+a = 3
+b = 2
+
+m = 2
+n = 3.6
+
+x = 3
+y = 0.2
+
+print_results(a, b)
